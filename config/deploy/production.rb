8a90c983fa7e0da62d5132b4ddaebc1917fb0b9e
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-10-18 21:19:40 +0530

    ssh option on server-2 configured

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -41,11 +41,11 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
+  set :ssh_options, {
+	  keys: %w(secrets/server-2-ssh.key),
+    forward_agent: false,
 #    auth_methods: %w(password)
-#  }
+  }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
