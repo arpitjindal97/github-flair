72e7c1cff624b6bb7d1158117dd30bc675dd299c
Arpit Agarwal <arpitjindal1997@hotmail.com>
2017-10-20 18:24:42 +0530

    download images before server start

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 func main() {
 
 	github_flair.SetTorProxy()
+	github_flair.DownloadImages()
 
 	http.HandleFunc("/AMT", amt)
 	http.HandleFunc("/github.png",github_flair.Flair)
@@ -21,7 +22,6 @@ func main() {
 	http.ListenAndServeTLS(":8080", "certificate.pem",
 		"private.key", nil)
 
-	github_flair.DownloadImages()
 }
 
 type Entry struct {
